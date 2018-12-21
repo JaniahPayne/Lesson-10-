acac2f01e73f0639ca2993afe3829a182f17648d
JaniahPayne <44148033+JaniahPayne@users.noreply.github.com>
2018-12-21 09:36:14 -0500

    Add files via upload

---
+++ Lesson 10/problem1.py
@@ -0,0 +1,12 @@
+print('-' * 65)
+print('Smart Bot: ')
+print()
+
+Smart = input('Are you Smart? ')
+if Smart == 'Yes':
+	print('So am I' )
+
+else:
+	print('Good for you.')
+
+print('-' * 65)
---
+++ Lesson 10/problem3.py
@@ -0,0 +1,11 @@
+print('-'*65)
+print('Welcome to CLUB425, the most lit club in downtown Actvf. Before you can enter,I need you to answer some questons...:')
+print()
+
+age = input('What is your age today? ')
+if age >= '21':
+	print('Cool come on in!')
+else:
+	print('Your gonna need to back up. This club is 21+ only so find somewhere else to party or find out what robot punches feel like!')
+	
+print('-'*65)
